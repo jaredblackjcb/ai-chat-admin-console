7d408c34bf6d76c8eb9ddf89a0c1d892d94dbbdd
Jared Black <jaredblackjcb@gmail.com>
2023-03-05 22:29:45 -0600

    Rewrite routes to restrict views based on authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, Outlet, RouterProvider, useLocation } from "react-router-dom";
+import { Routes, Navigate, Outlet, useLocation, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import Home from "./screens/Home";
 import Signup from "./screens/Signup";
@@ -10,40 +10,96 @@ import ManageSubscription from "./screens/ManageSubscription";
 import Profile from "./screens/Profile";
 import Header from "./components/Header";
 import { store } from "./store/index";
+import { useSelector } from "react-redux";
 
-const Layout = ({ hideHeaderOnPaths = [] }) => {
-  const { pathname } = useLocation();
-  return (
-    <>
-      {!hideHeaderOnPaths.includes(pathname) && <Header />}
-      <Outlet />
-    </>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    element: <Layout hideHeaderOnPaths={["/signup", "/login"]} />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/signup", element: <Signup /> },
-      { path: "/login", element: <Login /> },
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/profile", element: <Profile /> },
-      { path: "/subscription", element: <ManageSubscription /> },
-      { path: "/security/password", element: <ChangePassword /> },
-      // { path: "/checkout/success", element: <ChangePassword /> },
-      // { path: "/checkout/canceled", element: <ChangePassword /> },
-    ],
-  },
-]);
-
-function App() {
+export default function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route
+            path="login"
+            element={
+              <RestrictForAuth>
+                <Login />
+              </RestrictForAuth>
+            }
+          />
+          <Route path="" element={<h1>Under Construction</h1>} />
+          <Route element={<RootLayout />}>
+            <Route
+              path="dashboard"
+              element={
+                <RequireAuth>
+                  <Dashboard />
+                </RequireAuth>
+              }
+            ></Route>
+            <Route
+              path="profile"
+              element={
+                <RequireAuth>
+                  <Profile />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="subscription"
+              element={
+                <RequireAuth>
+                  <ManageSubscription />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="security/password"
+              element={
+                <RequireAuth>
+                  <ChangePassword />
+                </RequireAuth>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
 
-export default App;
+const RequireAuth = ({ children }) => {
+  const { userInfo } = useSelector((state) => state.userLogin) || {};
+  let location = useLocation();
+  console.log("requireAuth triggered", location, userInfo);
+  // Redirect them to the /login page, but save the current location they were
+  // trying to go to when they were redirected. This allows us to send them
+  // along to that page after they login, which is a nicer user experience
+  // than dropping them off on the home page.
+  console.log("redirect to login");
+  console.log(location.state);
+  return userInfo ? children : <Navigate to="/login" state={{ from: location }} replace />;
+};
+
+const RestrictForAuth = ({ children }) => {
+  const { userInfo } = useSelector((state) => state.userLogin) || {};
+  const location = useLocation();
+  console.log("location.state", location.state);
+  let { pathname } = location.state?.from || { pathname: "/dashboard" };
+  console.log("pathname: " + pathname);
+  if (pathname.includes("login")) {
+    pathname = "/dashboard";
+    console.log("pathname: " + pathname);
+  }
+  console.log("pathname: " + pathname);
+  return userInfo ? <Navigate to={pathname} state={{ from: location }} replace /> : children;
+};
+
+const RootLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
