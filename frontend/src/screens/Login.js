c3e56d5a838da3292b1c0d2d8d3e969478bdca38
Jared Black <=>
2023-08-22 21:17:04 -0500

    Convert login to mui components

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,72 +1,82 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button, Row, Col, Form, FormControl, Image, Card } from "react-bootstrap";
 import { login } from "../actions/userActions";
 import { useDispatch, useSelector } from "react-redux";
 
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const { error, loading } = useSelector((state) => state.user) || {};
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(login(email, password));
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    dispatch(login(data.get("email"), data.get("password")));
   };
 
   return (
-    <Container fluid>
+    <Container component="main" maxWidth="xs">
       {/* TODO: Add custom error and loader components */}
       {error && <h1>{error}</h1>}
       {loading && <h1>Loading...</h1>}
-      <Row>
-        <Col xs={12} md={7} lg={5} xl={4} className="d-flex align-items-center justify-content-center">
-          <Card className="card-plain mt-6">
-            <Card.Header>
-              <h3 className="text-center mb-2">Login</h3>
-            </Card.Header>
-            <Card.Body>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3 input-group input-group-static" controlId="formBasicEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    placeholder="Email address"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <FormControl.Feedback type="invalid"></FormControl.Feedback>
-                </Form.Group>
-
-                <Form.Group className="mb-3 input-group input-group-static" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-                </Form.Group>
-                <Button type="submit" variant="primary" className="w-100" onClick={handleSubmit}>
-                  Login
-                </Button>
-              </Form>
-            </Card.Body>
-            <Card.Footer>
-              <p className="mt-2">
-                Don't have account?{" "}
-                <Link className="text-primary text-gradient font-weight-bold" to={"/signup"}>
-                  Signup
-                </Link>
-              </p>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to={"/resetPassword"}>Forgot password?</Link>
+            </Grid>
+            <Grid item>
+              <Link to={"/signup"}>{"Don't have an account? Sign Up"}</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Container>
   );
 }
